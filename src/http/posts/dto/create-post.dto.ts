73525fcfcc75d60cc4f4d18b19773c481d8de75e
leo <kuzmichleva1@gmail.com>
2025-11-01 21:24:07 +0100

    create functionality for creating a new post and returning all from database

--- src/http/posts/dto/create-post.dto.ts
+++ src/http/posts/dto/create-post.dto.ts
@@ -1,1 +1,5 @@
-export class CreatePostDto {}
+export class CreatePostDto {
+  name: string;
+  description: string;
+  creator_id: string;
+}
--- src/http/posts/posts.service.ts
+++ src/http/posts/posts.service.ts
@@ -1,15 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PostEntity } from 'src/entities/post.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class PostsService {
-  create(createPostDto: CreatePostDto) {
-    return 'This action adds a new post';
+  constructor(
+    @InjectRepository(PostEntity)
+    private postRepository: Repository<PostEntity>,
+  ) {}
+
+  async create(createPostDto: CreatePostDto): Promise<CreatePostDto> {
+    const post = this.postRepository.create(createPostDto);
+    await this.postRepository.save(post);
+    return createPostDto;
   }
 
   findAll() {
-    return `This action returns all posts`;
+    return this.postRepository.find();
   }
 
   findOne(id: number) {
