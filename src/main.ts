dc5fcbd44f10af0ef1c52bf4ce816c272f4024df
leo <kuzmichleva1@gmail.com>
2025-11-01 16:25:06 +0100

    run specified port by node env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+console.log('NODE_ENV is:', process.env.NODE_ENV);
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
